4214aa00b956e21e40fcfc7a39ae695fa4776e37
tomjes03 <sonofdson@gmail.com>
2022-04-10 20:39:17 -0700

    My 10th task commit message

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: 0 Always (Success)
+ */
+int main(void)
+{
+int i, j;
+for (i = 48; i <= 56; i++)
+{
+for (j = 49; j <= 57; j++)
+{
+if (i < j)
+{
+putchar(i);
+putchar(j);
+if (i == 56 && j == 57)	
+continue; putchar(',');
+putchar(' ');
+}
+}
+}
+putchar('\n');
+return (0);
+}
