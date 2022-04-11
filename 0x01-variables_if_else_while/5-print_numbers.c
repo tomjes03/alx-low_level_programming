d6439766c911af49ad11f887ac2224e3c50ff495
tomjes03 <sonofdson@gmail.com>
2022-04-10 18:56:22 -0700

    My 5th task commit message

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: 0 Always (Success)
+ */
+int main(void)
+{
+int n;
+while (n < 10)
+{
+printf("%d", n);
+n++;
+}
+printf("\n");
+return (0);
+}
