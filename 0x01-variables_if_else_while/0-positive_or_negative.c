ff32d77dc6713e7b39565de09593a05ddb7bf82d
tomjes03 <sonofdson@gmail.com>
2022-04-10 18:20:55 -0700

    My first task commit message

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -14,5 +14,7 @@ if (n > 0)
 printf("%d is positive\n", n);
 else if (n == 0)
 printf("%d is zero\n", n);
-else										printf("%d is negative\n", n);							return (0);
+else
+printf("%d is negative\n", n);
+return (0);
 }
