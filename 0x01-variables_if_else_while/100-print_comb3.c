809540cb3a36fd05329c8ddf663d590b26910318
tomjes03 <sonofdson@gmail.com>
2022-04-10 21:28:20 -0700

    My 10th task commit message

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
- * main - Entry point
+ * main - Entry Point
  * Return: 0 Always (Success)
  */
 int main(void)
@@ -8,13 +8,13 @@ int main(void)
 int i, j;
 for (i = 48; i <= 56; i++)
 {
-for (j = 49; j <= 57; j++)
+for (j = 49; j <= 57; j++
 {
-if (i < j);
+if (i < j)
 {
 putchar(i);
 putchar(j);
-if (i == 56 && j == 57)	
+if (i == 56 && j == 57)
 continue;
 putchar(',');
 putchar(' ');
