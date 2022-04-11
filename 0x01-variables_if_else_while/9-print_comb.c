6f328af6ce09f24a6d007978c77496ff5e842bc2
tomjes03 <sonofdson@gmail.com>
2022-04-10 20:13:26 -0700

    My 9th task commit message

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry Point
+ * Return: 0 Always (Success)
+ */
+int main(void)
+{
+int n;
+for (n = 48; n < 58; n++)
+{
+putchar(n);	if (n != 57)
+{
+putchar(',');
+putchar(' ');
+}
+}
+putchar('\n');
+return (0);
+}
