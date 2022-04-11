1e6574aaef75fcd4078a3eb10f4a342ec5a2572e
tomjes03 <sonofdson@gmail.com>
2022-04-10 18:50:28 -0700

    My 4th task commit message

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: 0 Always (Success)
+ */
+int main(void)
+{
+char ch;
+for (ch = 'a'; ch <= 'z'; ch++)
+{
+if (ch == 'e' || ch == 'q')
+continue;
+putchar(ch);
+}
+putchar('\n');
+return (0);
+}
