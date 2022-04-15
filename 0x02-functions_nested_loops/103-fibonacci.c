a7eca29905fff3b8e241df6820af9fa58c17f1f0
tomjes03 <sonofdson@gmail.com>
2022-04-15 03:09:56 -0700

    My second to the last commit message

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -7,17 +7,16 @@
  */
 int main(void)
 {
-	int a = 0, b = 1, next = 0;
-	int sum = 0;
-
-	while (next < 4000000)
-	{
-		next = a + b;
-		a = b;
-		b = next;
-		if (next % 2 == 0)
-			sum += next;
-	}
-	printf("%i\n", sum);
-	return (0);
+int a = 0, b = 1, next = 0;
+int sum = 0;
+while (next < 4000000)
+{
+next = a + b;
+a = b;
+b = next;
+if (next % 2 == 0)
+sum += next;
+}
+printf("%i\n", sum);
+return (0);
 }
