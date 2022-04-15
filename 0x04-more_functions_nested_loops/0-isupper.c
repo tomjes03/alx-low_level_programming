7ac410ea078ad7cd1a91281b5a021682c863ae0c
Thomas Jesus <31595565+tomjes03@users.noreply.github.com>
2022-04-15 18:43:45 -0500

    Update 0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,11 +1,14 @@
 #include "main.h"
-
 /**
- * _isupper - checks for uppercase character
- * @letter: the character to be tracked
- * Return: 1 if c is uppercase, 0 otherwise
- */
-int _isupper(int letter)
+*_isupper - function that verifies if a  character is uppercase or not
+*@c: tested character
+*Return: returns 1 if it is uppercase , 0 if not
+*/
+
+int _isupper(int c)
 {
-	return (letter >= 'A' && letter <= 'Z');
+if ((c >= 'A') && (c <= 'Z'))
+return (1);
+
+return (0);
 }
