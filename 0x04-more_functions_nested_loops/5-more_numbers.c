98eb03663e688088b421cdc6dd93f625558d7a2c
tomjes03 <sonofdson@gmail.com>
2022-04-18 11:17:59 -0700

    My 5th task commit message

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -9,8 +9,10 @@ for (c = 0; c < 10; c++)
 {
 for (x = 0; x <= 14; x++)
 {
-if (x > 9)																{																	_putchar((x / 10) + 48);
+if (x > 9)
 {
+_putchar((x / 10) + 48);
+}
 _putchar((x % 10) + 48);
 }
 _putchar(10);
