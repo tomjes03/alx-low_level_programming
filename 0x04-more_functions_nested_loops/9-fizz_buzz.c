68bafdeddafefa4052c6db92e3e6d03a5791e163
tomjes03 <sonofdson@gmail.com>
2022-04-18 13:51:25 -0700

    My 9th task commit message

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,6 +1,5 @@
 #include "main.h"
 #include <stdio.h>
-
 /**
  * main - prints the numbers from 1 to 100, followed by a new line
  * but for multiples of three prints Fizz instead of the number
@@ -13,7 +12,6 @@ int i;
 for (i = 1; i <= 100; i++)
 {
 if (i % 3 == 0 && i % 5 != 0)
-{
 printf(" Fizz");
 } else if (i % 5 == 0 && i % 3 != 0)
 {
@@ -21,7 +19,7 @@ printf(" Buzz");
 } else if (i % 3 == 0 && i % 5 == 0)
 {
 printf(" FizzBuzz");
-} else if (i == 1);
+} else if (i == 1)
 {
 printf("%d", i);
 } else
