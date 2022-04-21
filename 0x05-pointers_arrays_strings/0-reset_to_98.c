4af53da9c1bdf222392446c2283932650a93fa44
tomjes03 <sonofdson@gmail.com>
2022-04-20 17:15:56 -0700

    My strings task commit message

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,12 +1,13 @@
 #include "main.h"
 
 /**
- *  * reset_to_98 - Updates the value of the parameter to 98
- *   * @n: The value
- *    *
- *     * Return: None
- *      */
+ * reset_to_98 - Updates the value of the parameter to 98
+ * @n: The value
+ *
+ * Return: None
+ */
 void reset_to_98(int *n)
 {
-		*n = 98;
+*n = 98;
 }
+
