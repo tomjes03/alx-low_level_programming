0be8ec5a3b166afb13fb23fd3503dc2d7acdee56
tomjes03 <sonofdson@gmail.com>
2022-04-24 07:00:30 -0700

    My 1st task commit message

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,30 +1,26 @@
 #include "main.h"
-
 /**
- * * _strncat - Concatenates two stings
- * * @dest: Destination string
- * * @src: the source of the string
- * * @n: number of bytes to look
- * * Return: Returns a pointer to the resulting string dest
- * */
-
+ * _strncat - concatenates n bytes from a string to another
+ * @dest: destination string
+ * @src: source string
+ * @n: number of bytes of str to concatenate
+ *
+ * Return: a pointer to the resulting string dest
+ */
 char *_strncat(char *dest, char *src, int n)
 {
-	int i;
-	int j;
-
-	i = 0;
-	while (dest[i] != '\0')
-	{
-		i++;
-	}
-	j = 0;
-	while (j < n && src[j] != '\0')
-	{
-		dest[i] = src[j];
-		i++;
-		j++;
-	}
-	dest[i] = '\0';
-	return (dest);
+int i, j;
+i = 0;
+j = 0;
+while (dest[i] != '\0')
+i++;
+while (src[j] != '\0' && j < n)
+{
+dest[i] = src[j];
+i++;
+j++;
 }
+dest[i] = '\0';
+return (dest);
+}
+
