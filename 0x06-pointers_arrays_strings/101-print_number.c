7f8445aba988b7796f25996230fe9b70ea489f35
tomjes03 <sonofdson@gmail.com>
2022-04-24 15:27:54 -0700

    My 9th task commit message

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,12 +1,13 @@
 #include "main.h"
+
 /**
  * print_number - prints an integer;
  * @n: integer to be printed;
  */
+
 void print_number(int n)
 {
 unsigned int n1;
-
 if (n < 0)
 {
 n1 = -n;
@@ -15,9 +16,8 @@ _putchar('-');
 {
 n1 = n;
 }
-
 if (n1 / 10)
 print_number(n1 / 10);
-
 _putchar((n1 % 10) + '0');
 }
+
