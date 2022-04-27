f63085deb9a2c3b56bd4ccf771a1167a23d69f8c
tomjes03 <sonofdson@gmail.com>
2022-04-26 18:07:51 -0700

    My Main.h task commit message

--- 0x07-pointers_arrays_strings/Main.h
+++ 0x07-pointers_arrays_strings/Main.h
@@ -3,10 +3,10 @@
 #include <stddef.h>
 #include <stdio.h>
 /**
- * *void prototypes(void)
- * *int prototypes(int)
- * *char prototypes(char)
- * */
+ *void prototypes(void)
+ *int prototypes(int)
+ *char prototypes(char)
+ */
 void print_chessboard(char (*a)[8]);
 void print_diagsums(int *a, int size);
 void set_string(char **s, char *to);
@@ -18,3 +18,4 @@ char *_strchr(char *s, char c);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
 #endif /* MAIN_H */
+
