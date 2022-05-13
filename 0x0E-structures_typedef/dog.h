c88fa6aedd169b21d9c44124d7a0651e068e4aa7
tomjes03 <sonofdson@gmail.com>
2022-05-12 17:22:13 -0700

    My first tast commit message

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,21 +1,24 @@
-#include "dog.h"
+#ifndef DOG_H
+#define DOG_H
 
 /**
- * init_dog - initializes a dog
- * @d: the dog to init
- * @name: the dog's name
+ * struct dog - a dog struct
+ * @name: dog's name
  * @age: dog's age
  * @owner: owner's name
  *
- * Return: void.
+ * Description: just a lone dog struct in a big kitty world
  */
-void init_dog(struct dog *d, char *name, float age, char *owner)
+struct dog
 {
-if (d)
-{
-d->name = name;
-d->age = age;
-d->owner = owner;
-}
-}
+char *name;
+float age;
+char *owner;
+};
+typedef struct dog dog_t;
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 
+#endif
