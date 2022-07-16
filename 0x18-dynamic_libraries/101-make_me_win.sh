c51cf88d248aabfea0899dee9bd91a2b33ddae5c
tomjes03 <sonofdson@gmail.com>
2022-07-15 23:49:48 -0700

    My dynamic libraries task commit message

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp https://github.com/speak2debby/alx-low_level_programming/blob/master/0x18-dynamic_libraries/101-make_me_win.sh
-export LD_PRELOAD=/tmp/libmask.so
+wget -P /tmp/ https://raw.github.com/Aysuarex/alx-low_level_programming/master/0x18-dynamic_libraries/nrandom.so
+export LD_PRELOAD=/tmp/nrandom.so
